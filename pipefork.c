886f921ae14ca75173a6200880364d4c66d4266a
Alice Vang <alicevang18@gmail.com>
2025-11-06 03:58:28 +0000

    2nd commit, testing 1 pipe

--- pipefork.c
+++ pipefork.c
@@ -8,9 +8,34 @@
 #define WRITE_END 1
 
 int main(void){
+	char write_size[BUFFER_SIZE] = "Hello World!\n";
+	char read_size[BUFFER_SIZE];
+	pid_t pid;
+
 	int fd[2];
 	if(pipe(fd) == -1){
 		fprintf(stderr,"Error: pipe failed.\n");
 		return 1;
 	}
+
+	pid = fork();
+
+	if(pid < 0){
+		fprintf(stderr, "Error: fork failed.\n");
+		return 1;
+	}
+
+	if(pid > 0){
+		close(fd[READ_END]);	// close the unused end of the pipe
+		write(fd[WRITE_END], write_size, strlen(write_size)+1); // write to the pipe
+		close(fd[WRITE_END]); // close the write end of the pipe
+	} else {
+		close(fd[WRITE_END]); // close the unused end of the pipe
+		read(fd[READ_END], read_size, BUFFER_SIZE); // read from the pipe
+		printf("read %s", read_size);
+		
+		close(fd[READ_END]);
+	}
+
+	return 0;
 }
