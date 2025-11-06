4bfcd0e8aaee25f34536287b96f511f3574fcb09
Alice Vang <alicevang18@gmail.com>
2025-11-06 23:23:05 +0000

    fixing outputs

--- pipefork.c
+++ pipefork.c
@@ -33,25 +33,31 @@ int main(void){
 
 	if(pid > 0){ // parent process
 		close(parent_fd[READ_END]);	// close the unused end of the parent pipe
+		close(child_fd[WRITE_END]);	// close child write pipe end
+
 		write(parent_fd[WRITE_END], parent_write, strlen(parent_write)+1); // write to the parent pipe
-		close(parent_fd[WRITE_END]); // close the write end of the parent pipe
-	//	printf("%s %d\n", parent_read, getpid());
+		close(parent_fd[WRITE_END]); // close the write end of the parent pipe after writing
 		
-		close(child_fd[WRITE_END]);
 		read(child_fd[READ_END], child_read, BUFFER_SIZE);
+		printf("%s %d\n", child_read, BUFFER_SIZE);
+		close(child_fd[READ_END]); // close after reading
 
 	} else { //child process
 		close(child_fd[READ_END]);
+		close(parent_fd[WRITE_END]); // close unused end of the pipe
+
+		read(parent_fd[READ_END], parent_write, BUFFER_SIZE);
+		printf("%s %d\n", parent_read, BUFFER_SIZE);
+	
 		write(child_fd[WRITE_END], child_write, strlen(child_write)+1);
 		close(child_fd[WRITE_END]);
 
-		close(parent_fd[WRITE_END]); // close the unused end of the pipe
-		read(parent_fd[READ_END], parent_read, BUFFER_SIZE); // read from the pipe
+	//	read(parent_fd[READ_END], parent_read, BUFFER_SIZE); // read from the pipe
 	//	printf("%s %d\n", parent_read, getpid());
 		
 		close(parent_fd[READ_END]);
 	}
-	printf("%s %d\n", parent_read, getppid());
-	printf("%s %d\n", child_read, getpid());
+	//printf("%s %d\n", parent_read, getppid());
+	//printf("%s %d\n", child_read, getpid());
 	return 0;
 }
